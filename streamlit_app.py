0dc9e1eb2c8daa93e89f663766299e46be84bfa9
Subhash Bohra <subhash.bohra@gmail.com>
2023-01-17 12:45:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 import pandas
 
-
-
 streamlit.title("My Parents New Healthy Menu")
 streamlit.subheader("Breakfast Menu")
 streamlit.write("🥣 Omega 3 and Blueberry Oatmeal \n")
@@ -17,7 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) 
 
 # Display the table on the page  
 
