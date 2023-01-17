6beecf84f66375fd64a0b29779733967fd85d7ea
Subhash Bohra <subhash.bohra@gmail.com>
2023-01-17 12:34:40 +0530

    Update streamlit_app.py
    added emojis and new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.title("My Parents New Healthy Menu")
 streamlit.subheader("Breakfast Menu")
-streamlit.write("Omega 3 and Blueberry Oatmeal \n")
-streamlit.write("Kale, Spinach & Rocket Smoothie \n")
-streamlit.write("Hard-Boild Free-Range Egg")
+streamlit.write("🥣 Omega 3 and Blueberry Oatmeal \n")
+streamlit.write("🥗 Kale, Spinach & Rocket Smoothie \n")
+streamlit.write("🐔 Hard-Boild Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast\n")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
