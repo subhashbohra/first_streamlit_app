33284d319f16bad179235d03b778120b34e8cbbc
Subhash Bohra <subhash.bohra@gmail.com>
2023-01-17 12:31:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 streamlit.title("My Parents New Healthy Menu")
 streamlit.subheader("Breakfast Menu")
-streamlit.write("Omega 3 and Blueberry Oatmeal")
+streamlit.write("Omega 3 and Blueberry Oatmeal \n")
+streamlit.write("Kale, Spinach & Rocket Smoothie \n")
+streamlit.write("Hard-Boild Free-Range Egg")
+
